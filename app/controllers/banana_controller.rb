f7e94715c15f3157333846fc4fe69f716e0048cb
yoojinn93 <yoojinn93@naver.com>
2016-03-31 11:04:04 +0000

    1

---
+++ app/controllers/banana_controller.rb
@@ -0,0 +1,6 @@
+class BananaController < ApplicationController
+    def apple
+        @mango = "안녕하세요호 전 목감기"
+        @mini = "I LUV YOU"
+    end
+end
---
+++ test/controllers/banana_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BananaControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
